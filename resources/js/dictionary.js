358445a847e57e944e6be2d00b12ea782f88c9c8
Drausk2 <drausk2@gmail.com>
2024-01-04 14:04:48 +0100

    added languges

---
+++ resources/js/dictionary.js
@@ -0,0 +1,41 @@
+var dictionary = {
+    'top100': {
+        'de': 'Wähle aus den top 100 verwendeten Memes',
+        'en': 'Select from the top 100 memes frequently used',
+    },
+    'or': {
+        'de': 'oder',
+        'en': 'or',
+    },
+    'files1': {
+        'de': 'Ziehe ein Bild hierher, oder',
+        'en': 'Drag and drop a image here, or',
+    },
+    'files2': {
+        'de': 'durchsuchen',
+        'en': 'browse',
+    }, 
+    'files3': {
+        'de': 'den Computer.',
+        'en': 'your computer.',
+    },    
+};
+var langs = ['de', 'en'];
+var langnum = '2'
+var current_lang_index = 0;
+var current_lang = langs[current_lang_index];
+
+window.change_lang = function() {
+current_lang_index = ++current_lang_index % langnum;
+current_lang = langs[current_lang_index];
+translate();
+}
+
+function translate() {
+$("[data-translate]").each(function(){
+    var key = $(this).data('translate');
+    $(this).html(dictionary[key][current_lang] || "N/A");
+});
+}
+
+translate();

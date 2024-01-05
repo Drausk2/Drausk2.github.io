e67e2afdff029c040da2f4ab83a6fc4c3b59cf1f
Drausk2 <drausk2@gmail.com>
2024-01-05 13:37:07 +0100

    Change language functionality update.

--- resources/js/dictionary.js
+++ resources/js/dictionary.js
@@ -21,21 +21,29 @@ var dictionary = {
     },    
 };
 var langs = ['de', 'en'];
-var langnum = '2'
+var langnum = '2';
 var current_lang_index = 0;
 var current_lang = langs[current_lang_index];
 
 window.change_lang = function() {
-current_lang_index = ++current_lang_index % langnum;
-current_lang = langs[current_lang_index];
-translate();
+    current_lang_index = ++current_lang_index % langnum;
+    current_lang = langs[current_lang_index];
+    translate();
 }
 
 function translate() {
-$("[data-translate]").each(function(){
-    var key = $(this).data('translate');
-    $(this).html(dictionary[key][current_lang] || "N/A");
-});
+    $("[data-translate]").each(function(){
+        var key = $(this).data('translate');
+        $(this).html(dictionary[key][current_lang] || "N/A");
+    });
+
+    // Save the current language to local storage
+    localStorage.setItem('current_lang', current_lang);
+}
+
+// Check if there is a stored language in local storage
+if (localStorage.getItem('current_lang')) {
+    current_lang = localStorage.getItem('current_lang');
 }
 
 translate();
